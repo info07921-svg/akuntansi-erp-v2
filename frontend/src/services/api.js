9f12c4b85e5679294bcd951ac786f2394c1c6065
DevStudio <info07921@gmail.com>
2026-07-24 15:06:50 +0700

    update api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://akuntansi-erp-v2-production.up.railway.app",
 });
 
 // REQUEST INTERCEPTOR — selipkan token di setiap request
